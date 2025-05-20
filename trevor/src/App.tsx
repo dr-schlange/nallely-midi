e69ce7f8074fe1eb364fde210167b55fad262646
dr-schlange <drcoatl@proton.me>
2025-05-19 20:05:10 -0600

    Change centering of modules in rack

--- trevor/src/App.tsx
+++ trevor/src/App.tsx
@@ -25,6 +25,62 @@ const Main = () => {
 				<DevicePatching />
 			</div>
 			{errors && errors.length > 0 && <ErrorModal errors={errors} />}
+			<svg>
+				<title>Global definitions</title>
+				<defs>
+					<marker
+						id="retro-arrowhead"
+						markerWidth="6"
+						markerHeight="6"
+						refX="5"
+						refY="3"
+						orient="auto"
+						markerUnits="strokeWidth"
+					>
+						<polygon
+							points="0,0 5,3 0,6"
+							fill="gray"
+							stroke="white"
+							strokeWidth="1"
+							strokeOpacity="0.3"
+						/>
+					</marker>
+					<marker
+						id="selected-retro-arrowhead"
+						markerWidth="6"
+						markerHeight="6"
+						refX="5"
+						refY="3"
+						orient="auto"
+						markerUnits="strokeWidth"
+					>
+						<polygon
+							points="0,0 5,3 0,6"
+							fill="blue"
+							stroke="white"
+							strokeWidth="1"
+							strokeOpacity="0.8"
+						/>
+					</marker>
+					<marker
+						id="bouncy-retro-arrowhead"
+						markerWidth="6"
+						markerHeight="6"
+						refX="5"
+						refY="3"
+						orient="auto"
+						markerUnits="strokeWidth"
+					>
+						<polygon
+							points="0,0 5,3 0,6"
+							fill="green"
+							stroke="white"
+							strokeWidth="1"
+							strokeOpacity="0.8"
+						/>
+					</marker>
+				</defs>
+			</svg>
 		</div>
 	);
 };
