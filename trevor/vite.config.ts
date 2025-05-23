bfabfd2f82b1641c0417601a63c5da0b5448f8d6
dr-schlange <drcoatl@proton.me>
2025-05-22 18:17:05 -0600

    Prepare GH deployment for Trevor

--- trevor/vite.config.ts
+++ trevor/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+const command = process.env.npm_lifecycle_event;
+
+const sourcemap = command !== "deploy-gh";
+const base = command === "deploy-gh" ? "/nallely-midi/" : "/";
+
 export default defineConfig({
+	base,
+	build: {
+		outDir: "dist",
+		sourcemap: sourcemap,
+	},
 	plugins: [react()],
 });
