db112bb21871407b581fe9c5baa376e19254aaae
dr-schlange <drcoatl@proton.me>
2025-06-22 10:16:33 -0600

    Improve pitch shifter implementation with new API

--- nallely/experimental/harmonizer.py
+++ nallely/experimental/harmonizer.py
@@ -1,20 +1,28 @@
 from decimal import Decimal
 
-from nallely import ThreadContext, VirtualDevice, VirtualParameter
+from nallely import ThreadContext, VirtualDevice, VirtualParameter, on
 
 
 class PitchShifter(VirtualDevice):
-    output_cv = VirtualParameter("output", range=(0, 127))
-    input_cv = VirtualParameter("input", range=(0, 127))
-    shift_cv = VirtualParameter("shift", range=(-48, +48))
+    input_cv = VirtualParameter("input", range=(0, 128))
+    shift_cv = VirtualParameter("shift", range=(-24, +24))
+
+    @property
+    def range(self):
+        return (0, 127)
 
     def __init__(self, *args, **kwargs):
         self.input = None
         self.shift = 0
         super().__init__(*args, **kwargs)
 
-    def main(self, ctx: ThreadContext):
-        return self.input + self.shift if self.input else None
+    @on(input_cv, edge="both")
+    def shift_input(self, value, ctx):
+        return value + self.shift
+
+    @on(input_cv, edge="falling")
+    def reset_input(self, value, ctx):
+        return 0
 
 
 class Harmonizer(VirtualDevice):
