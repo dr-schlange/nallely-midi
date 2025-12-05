c20eea0f77c698e94e58cdc5aaa45d519d248d87
dr-schlange <drcoatl@proton.me>
2025-12-05 12:48:24 -0600

    We change phase

--- nallely/cli.py
+++ nallely/cli.py
@@ -7,7 +7,7 @@ def parse_args(argv):
     parser = argparse.ArgumentParser(
         prog="nallely",
         description="""Modular MIDI brain for MIDI instruments that let's you focus on your device, not the exchanged MIDI messages""",
-        epilog="Current phase: Tlitliltzin",
+        epilog="Current phase: Tepezcohuite",
     )
     subparsers = parser.add_subparsers(dest="command", required=False)
     run_parser = subparsers.add_parser(
