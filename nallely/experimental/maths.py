2258ed416d5b4c64a118cbbf5f5d411c7a1c6c29
dr-schlange <drcoatl@proton.me>
2026-01-17 10:38:02 -0600

    Change integrator bounds and add initial value

--- nallely/experimental/maths.py
+++ nallely/experimental/maths.py
@@ -728,50 +728,51 @@ class Integrator(VirtualDevice):
     non-determinism in async feedback loops.
 
     inputs:
-    * input_cv [-1.0, 1.0] init=0.0 round: Integrator input in integrator unit
+    * input_cv [None, None] init=0.0 round: Integrator input in integrator unit
+    * initial_cv [-1.0, 1.0] init=0.0 <any>: Initial condition
     * gain_cv [0.0, 100.0] init=1.0: Integrator gain
     * reset_cv [0, 1.0] round: Reset integrator
 
     outputs:
-    * out_cv [-1.0, 1.0]: output
+    * output_cv [None, None]: main output
 
     type: continuous
     category: math
-    meta: disable default output
     """
 
-    input_cv = VirtualParameter(name="input", range=(None, None), default=0.0)
+    initial_cv = VirtualParameter(name="initial", range=(-1.0, 1.0), default=0.0)
+    input_cv = VirtualParameter(
+        name="input", range=(None, None), conversion_policy="round", default=0.0
+    )
     gain_cv = VirtualParameter(name="gain", range=(0.0, 100.0), default=1.0)
     reset_cv = VirtualParameter(
         name="reset", range=(0.0, 1.0), conversion_policy="round"
     )
-    out_cv = VirtualParameter(name="out")
 
     @property
     def range(self):
-        return None, None
+        return (None, None)
 
     def __post_init__(self, **kwargs):
-        self.value = 0.0
+        self.value = self.initial
         self.last_time = time.time()
-        return {"disable_output": True}
 
     def main(self, ctx: ThreadContext):
         now = time.time()
         dt = now - self.last_time
         self.last_time = now
-
-        self.value += (self.input * self.gain) * dt
-
-        if self.value > 1.0:
-            self.value = 1.0
-        elif self.value < -1.0:
-            self.value = -1.0
-
-        yield (self.value, [self.out_cv])
+        self.value += self.input * self.gain * dt
+        self.value = max(-1.0, min(1.0, self.value))
+        yield self.value
 
     @on(reset_cv, edge="rising")
     def on_reset(self, value, ctx):
-        self.value = 0.0
+        self.value = self.initial
         self.last_time = time.time()
-        return (0.0, [self.out_cv])
+        return self.value
+
+    @on(initial_cv, edge="any")
+    def on_initial_any(self, value, ctx):
+        self.value = value
+        self.last_time = time.time()
+        return self.value
