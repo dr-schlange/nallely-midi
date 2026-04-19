4beb60887ad944ce83c7e614fe894556411225b3
dr-schlange <drcoatl@proton.me>
2026-04-19 04:59:43 -0600

    Update Lisa config

--- nallely/experimental/lisa_pico.py
+++ nallely/experimental/lisa_pico.py
@@ -6,6 +6,7 @@ import nallely
 
 
 class GeneralSection(nallely.Module):
+    gain = nallely.ModuleParameter(3, init_value=32, description="General Gain")
     master_volume = nallely.ModuleParameter(7, description="General Volume")
     engine_select = nallely.ModuleParameter(8, description="Engine Selection")
     sustain = nallely.ModuleParameter(64, description="Sustain (Hold notes)")
