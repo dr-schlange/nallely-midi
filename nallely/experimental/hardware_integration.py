feef46d1774edcbd5511bc917290519ca9dcaf50
dr-schlange <drcoatl@proton.me>
2026-04-19 06:58:47 -0600

    Add shift support in LISA hw interface

--- nallely/experimental/hardware_integration.py
+++ nallely/experimental/hardware_integration.py
@@ -12,7 +12,7 @@ from nallely.experimental.lisa_pico import Lisa
 from nallely.experimental.Minilab3 import Minilab3
 
 
-@gencode(keep_decorator=True)
+# @gencode(keep_decorator=True)
 class LISA(VirtualDevice):
     """
     Interface with LISA, expose only few entries, spawns 4 LFOs and connects hardware. Manage lifecycles of each modules.
@@ -23,13 +23,14 @@ class LISA(VirtualDevice):
     * wt3_amplitude_cv [0, 1] init=1 <any>: amplitude for waveform of wavetable 3
     * wt4_amplitude_cv [0, 1] init=1 <any>: amplitude for waveform of wavetable 4
     * reconnect_cv [0, 1] init=0 <rising>: force a reconnection to MIDI devices
+    * shift_cv [0, 1] init=0 <rising, falling>: access to second layer of buttons/sliders
 
     type: ondemand
     category: hardware-integration
     meta: disable default output
     """
 
-    reconnect_cv = VirtualParameter(name="reconnect", range=(0.0, 1.0), default=0.0)
+    shift_cv = VirtualParameter(name="shift", range=(0.0, 1.0), default=0.0)
     wt1_amplitude_cv = VirtualParameter(
         name="wt1_amplitude", range=(0.0, 1.0), default=1.0
     )
@@ -42,8 +43,9 @@ class LISA(VirtualDevice):
     wt4_amplitude_cv = VirtualParameter(
         name="wt4_amplitude", range=(0.0, 1.0), default=1.0
     )
+    reconnect_cv = VirtualParameter(name="reconnect", range=(0.0, 1.0), default=0.0)
 
-    def _setup_lfos(self, lisa, minilab):
+    def _setup_lfos(self, lisa: Lisa, minilab: Minilab3):
         self.lfo1 = LFO(speed=1, sampling_rate=259, method="log")
         self.lfo2 = LFO(speed=1, sampling_rate=259, method="log")
         self.lfo3 = LFO(speed=1, sampling_rate=259, method="log")
@@ -51,6 +53,18 @@ class LISA(VirtualDevice):
         for i in range(1, 4):
             lfo = getattr(self, f"lfo{i}")
             lfo.set_parameter("auto_srate", "OFF")
+
+        # map non-shift needs to be declared first
+        lisa.general.master_volume = minilab.buttons.b1.scale(0, 127, method="log")
+        lisa.general.gain = minilab.buttons.b2.scale(0, 127, method="log")
+        lisa.envelope.attack = minilab.buttons.b3.scale(0, 127)
+        lisa.envelope.release = minilab.buttons.b4.scale(0, 127)
+        lisa.filter.cutoff = minilab.buttons.b5.scale(0, 127)
+        lisa.filter.resonance = minilab.buttons.b6.scale(0, 127)
+        lisa.filter.type = minilab.buttons.b7.scale(0, 127)
+        lisa.modulation.FM_mod = minilab.buttons.b8.scale(0, 127)
+
+        # map shifts
         self.lfo1.sampling_rate_cv = minilab.buttons.b1.scale(20, 1000, method="log")
         self.lfo2.sampling_rate_cv = minilab.buttons.b2.scale(20, 1000, method="log")
         self.lfo3.sampling_rate_cv = minilab.buttons.b3.scale(20, 1000, method="log")
@@ -59,6 +73,8 @@ class LISA(VirtualDevice):
         self.lfo2.speed_cv = minilab.buttons.b6.scale(0.5, 7, method="log")
         self.lfo3.speed_cv = minilab.buttons.b7.scale(0.5, 7, method="log")
         self.lfo4.speed_cv = minilab.buttons.b8.scale(0.5, 7, method="log")
+
+        # shift & non shift
         lisa.wavetable.stream_table1 = self.lfo1.scale(-8192, 8192)
         lisa.wavetable.stream_table2 = self.lfo2.scale(-8192, 8192)
         lisa.wavetable.stream_table3 = self.lfo3.scale(-8192, 8192)
@@ -68,6 +84,35 @@ class LISA(VirtualDevice):
         self.adsr_filter = ADSREnvelope()
         self.adsr_filter.gate_cv = minilab.keys.notes
         lisa.filter.cutoff = self.adsr_filter.output_cv.scale(0, 127)
+
+        # map non-shift needs to be declared first
+        self.adsr_filter.attack_cv = minilab.sliders.s1
+        self.adsr_filter.decay_cv = minilab.sliders.s2
+        self.adsr_filter.sustain_cv = minilab.sliders.s3
+        self.adsr_filter.release_cv = minilab.sliders.s4
+        self.adsr_filter.set_pause_cv = minilab.pads.p5
+
+    def collect_mappings(self, minilab: Minilab3):
+        non_shift_map = []
+        shift_map = []
+        btns = minilab.buttons
+        sliders = minilab.sliders
+        for i in range(1, 9):
+            src = getattr(btns, f"b{i}")
+            links = src.outgoing_links
+            non_shift_map.append(links[0])
+            shift_map.append(links[1])
+            links[1].muted = True
+
+        for i in range(1, 5):
+            src = getattr(sliders, f"s{i}")
+            links = src.outgoing_links
+            non_shift_map.append(links[0])
+            shift_map.append(links[1])
+            links[1].muted = True
+
+        self.non_shift_map = non_shift_map
+        self.shift_map = shift_map
 
     def __post_init__(self, **kwargs):
         lisa = Lisa(autoconnect=False)
@@ -89,6 +134,13 @@ class LISA(VirtualDevice):
         lisa.wavetable.freeze_wt2 = minilab.pads.p2
         lisa.wavetable.freeze_wt3 = minilab.pads.p3
         lisa.wavetable.freeze_wt4 = minilab.pads.p4
+        lisa.wavetable.reset_all_wt = minilab.pads.p8
+        lisa.wavetable.reset_all_write_idx = minilab.pads.p7
+
+        self._setup_lfos(lisa, minilab)
+        self._setup_adsr(lisa, minilab)
+
+        # map shifts
         self.wt1_amplitude_cv = minilab.sliders.s1.scale(65, 522, method="log")
         self.wt2_amplitude_cv = minilab.sliders.s2.scale(65, 522, method="log")
         self.wt3_amplitude_cv = minilab.sliders.s3.scale(65, 522, method="log")
@@ -96,8 +148,7 @@ class LISA(VirtualDevice):
         lisa.wavetable.reset_all_write_idx = "ON"
         lisa.wavetable.reset_all_write_idx = "OFF"
 
-        self._setup_lfos(lisa, minilab)
-        self._setup_adsr(lisa, minilab)
+        self.collect_mappings(minilab)
         return {"disable_output": True}
 
     def setup(self) -> ThreadContext:
@@ -105,7 +156,6 @@ class LISA(VirtualDevice):
         self.lfo2.start()
         self.lfo3.start()
         self.lfo4.start()
-        self.adsr_filter.start()
         return super().setup()
 
     def stop(self, clear_queues=True):
@@ -143,7 +193,21 @@ class LISA(VirtualDevice):
 
     @on(reconnect_cv, edge="rising")
     def on_reconnect_rising(self, value, ctx):
-        self.reconnect = 0  # force it back to 0
+        self.reconnect = 0
         self.lisa.reconnect_input(exact=False)
         self.lisa.reconnect_output(exact=False)
         self.minilab.reconnect_input(exact=False)
+
+    @on(shift_cv, edge="rising")
+    def on_shift_rising(self, value, ctx):
+        for link in self.non_shift_map:
+            link.muted = True
+        for link in self.shift_map:
+            link.muted = False
+
+    @on(shift_cv, edge="falling")
+    def on_shift_falling(self, value, ctx):
+        for link in self.non_shift_map:
+            link.muted = False
+        for link in self.shift_map:
+            link.muted = True
