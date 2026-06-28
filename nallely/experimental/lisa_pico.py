4aa7bbfc3f993d8b173dd418233e5548e7dd3555
dr-schlange <drcoatl@proton.me>
2026-06-28 08:15:37 -0600

    Update LISA config

--- nallely/experimental/lisa_pico.py
+++ nallely/experimental/lisa_pico.py
@@ -8,6 +8,7 @@ import nallely
 class GeneralSection(nallely.Module):
     gain = nallely.ModuleParameter(3, init_value=32, description="General Gain")
     master_volume = nallely.ModuleParameter(7, description="General Volume")
+    panning = nallely.ModuleParameter(14, init_value=64, description="Panning")
     engine_select = nallely.ModuleParameter(8, description="Engine Selection")
     voice_mode = nallely.ModuleParameter(
         2, description="Mode for the voices", accepted_values=["poly", "unison", "mono"]
