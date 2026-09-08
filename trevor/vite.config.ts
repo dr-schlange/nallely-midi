f8d225e7775af12d02ab1d0342431e54f18b2cfd
dr-schlange <drcoatl@proton.me>
2026-09-08 17:30:25 -0600

    Add experimental react compiler

--- trevor/vite.config.ts
+++ trevor/vite.config.ts
@@ -10,7 +10,7 @@ console.log(`** Building for base: ${base}`);
 export default defineConfig(({ mode }) => {
   const analyzeMode = mode === "analyze";
   const plugins = [
-    react(),
+    react({ compiler: true }),
     compression({
       algorithms: ["gzip"],
       threshold: 1024,
